2fb3b20c814fdb9ebd517fc062395000a07f1453
AlexLhui <93427480+AlexLhui@users.noreply.github.com>
2021-11-16 16:00:39 +0100

    Added Dice class Updated the Team class

--- SmallHorses/src/Team.java
+++ SmallHorses/src/Team.java
@@ -1,11 +1,24 @@
 public class Team {
 
-    private int NumberOfTeam;
+    private int numberOfTeam = 0;
     enum teamColor{Yellow, Blue, Green, Red}
-    teamColor team;
+    public teamColor team;
+
+    public int getNumberOfTeam() {
+        return numberOfTeam;
+    }
+
+    public void setNumberOfTeam(int nb) {
+        numberOfTeam = nb;
+    }
+
+    public void addTeam() {
+        setNumberOfTeam(getNumberOfTeam()+1);
+    }
 
     public Team (teamColor color) {
         this.team = color;
+        addTeam();
     }
 
 }
