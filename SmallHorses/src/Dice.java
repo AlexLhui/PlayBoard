56cbf33cc9e1e6dfd089a9e8b371a2ae30cb1438
AlexLhui <93427480+AlexLhui@users.noreply.github.com>
2021-11-16 16:23:19 +0100

    Updated the Dice class

--- SmallHorses/src/Dice.java
+++ SmallHorses/src/Dice.java
@@ -1,2 +1,24 @@
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Dice {
+
+    private int lastResult;
+
+    public int getLastResult() {
+        return lastResult;
+    }
+
+    public void setLastResult(int res) {
+        this.lastResult = res;
+    }
+
+    public int throwDice() {
+        int res = ThreadLocalRandom.current().nextInt(1,6+1);
+        setLastResult(res);
+        return res;
+    }
+
+    public Dice () {
+        this.lastResult = getLastResult();
+    }
 }
