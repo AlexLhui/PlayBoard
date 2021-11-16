13fd256a3b9ca46a16bfc3d6e7375b8c88a1ce85
Berillon <theoberillon4@gmail.com>
2021-11-16 16:58:40 +0100

    Bonjour

--- SmallHorses/src/Board.java
+++ SmallHorses/src/Board.java
@@ -2,4 +2,16 @@ public class Board {
 
     public final int nbCase = 72;
 
+    /**
+     * Squares 1 to 4 : Blue stable
+     * Squares 5 to 8 : Red stable
+     * Squares 9 to 12 : Green stable
+     * Squares 13 to 16 : Yellow stable
+     * Squares 17 to 56 : Game
+     * Squares 57 to 60 : Final arrival for blue team
+     * Squares 61 to 64 : Final arrival for red team
+     * Squares 65 to 68 : Final arrival for green team
+     * Squares 70 to 72 : Final arrival for yellow team
+     **/
+
 }
